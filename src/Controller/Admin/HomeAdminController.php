21525d87862a03d552e2437f0db733bf7799337e
crownbackend <crownbackend@gmail.com>
2018-12-16 18:12:30 +0100

    admin

--- src/Controller/Admin/HomeAdminController.php
+++ src/Controller/Admin/HomeAdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -12,5 +13,11 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class HomeAdminController extends AbstractController
 {
-
+    /**
+     * @Route("/", name="admin_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('backOffice/home/index.html.twig');
+    }
 }
