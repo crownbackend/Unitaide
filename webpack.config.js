4fc4919c1b9814efeaddaf3e035290c2f01cc0b6
crownbackend <crownbackend@gmail.com>
2018-12-17 19:18:26 +0100

    admin style add

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,8 @@ Encore
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+    .addEntry('uikit', './assets/js/uikit.js')
+    .addEntry('uikit-icon', './assets/js/uikit-icons.js')
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
